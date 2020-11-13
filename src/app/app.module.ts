6721ee1db9aa619adf271f78c8322a08f0b1d27b
luciaagarcia <lucia.alfonso@alu.uclm.es>
2020-11-13 21:49:24 +0100

    Video con tarjetas de información poco optimizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { VgCoreModule } from 'videogular2/compiled/core';
 import { VgControlsModule } from 'videogular2/compiled/controls';
+import {VgOverlayPlayModule} from 'videogular2/compiled/overlay-play';
+import {VgBufferingModule} from 'videogular2/compiled/buffering';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,8 +16,11 @@ import { VgControlsModule } from 'videogular2/compiled/controls';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     VgCoreModule,
-    VgControlsModule
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
